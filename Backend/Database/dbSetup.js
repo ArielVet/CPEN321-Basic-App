0ba77648a49e018cb602c518ca8bb6873fa49481
Ariel <arielvi2002@gmail.com>
2023-09-08 00:23:43 -0700

    Added Backend

---
+++ Backend/Database/dbSetup.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb://0.0.0.0:27017";
+const client = new MongoClient(uri);
+
+async function connect () {
+    try {
+        await client.connect();
+        console.log("Successfully connected to the database");  
+    }
+    catch (err) {
+        console.log(err);
+        await client.close();
+    }
+}
+
+module.exports = { connect };
+
