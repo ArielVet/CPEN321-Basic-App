f9eaf19781223cf5a874fcf7f832bae6be15fefe
Ariel <arielvi2002@gmail.com>
2023-09-08 17:43:07 -0700

    backend changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const db = require('./Database/dbSetup.js');
 
 app.use(express.json());
 
@@ -26,6 +25,4 @@ app.get('/name', (req, res) => {
 
 const server = app.listen(8081, () => {
     console.log('Server is running on port http://%s:%s', server.address().address, server.address().port);
-    db.connect();
-
 });
